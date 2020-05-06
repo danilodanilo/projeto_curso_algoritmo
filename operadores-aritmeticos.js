c4f924a5e5d3d9d045270f36dfe3a7797fd69dab
Your Name <you@example.com>
2020-05-06 14:08:33 -0300

    Removido comentários do fonte

--- operadores-aritmeticos.js
+++ operadores-aritmeticos.js
@@ -1,19 +1,3 @@
-/*
-Var
-// Seção de Declarações das variáveis 
-   resultado, valor01, valor02, valor03: real
-
-Inicio
-   // Seção de Comandos, procedimento, funções, operadores, etc...
-   escreval("Entre com os 3 valores")
-   leia(valor01)
-   leia(valor02)
-   leia(valor03)
-  // resultado := (valor01 - valor02) / valor03
-  //resultado := (valor01 + valor02) * valor03
-  resultado := ((valor01 + valor02 + valor02) / valor03) % 2
-   escreva("Resultado", resultado)
-*/
 var resultado, valor01, valor02, valor03;
 
 valor01 = 10
